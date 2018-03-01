7bb94780da8dccb856358cfb3151a7df33dc1294
flyinj <bryan.cormy@gmail.com>
2018-03-01 10:15:19 -0800

    Add .gitignore file

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "perso"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-40d28157"
+  ami           = "ami-40d28157"
   instance_type = "t2.micro"
 
   tags {
